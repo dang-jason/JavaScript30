37fd415a9d4031d6a1540145d9580eb6a7bccae0
Jason Dang <jasondang619@gmail.com>
2023-01-11 10:33:16 -0600

    Connected video to canvas with color filters

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,104 @@
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d', { willReadFrequently: true });
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            //  DEPRECIATION : 
+            //       The following has been depreceated by major browsers as of Chrome and Firefox.
+            //       video.src = window.URL.createObjectURL(localMediaStream);
+            //       Please refer to these:
+            //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+            //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+                  
+            video.srcObject = localMediaStream;
+            video.play();
+        }).catch(err => {
+            console.error(`OH NO!!!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        //get the pixels
+        let pixels = ctx.getImageData(0, 0, width, height);
+        //mess with the pixels
+
+        //makes cavas red
+        // pixels = redEffect(pixels); 
+
+        //rgb split canvas colors
+        // pixels = rgbSplit(pixels); 
+        // ctx.globalAlpha = .2;
+
+        //greenscreen the canvas
+        pixels = greenScreen(pixels);
+
+        //put them back
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+    //take the data from canvas to download
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a')
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels){
+    for(let i = 0; i < pixels.data.length; i+= 4){
+        pixels.data[i+0] = pixels.data[i+0] +200; //red
+        pixels.data[i+1] = pixels.data[i+1] - 50;//green
+        pixels.data[i+2] = pixels.data[i+2] * .5;//blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels){
+    for(let i = 0; i < pixels.data.length; i+= 4){
+        pixels.data[i-150] = pixels.data[i+0]; //red
+        pixels.data[i+200] = pixels.data[i+1];//green
+        pixels.data[i-250] = pixels.data[i+2];//blue
+    }
+    return pixels;
+}
+
+function greenScreen(pixels){
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4){
+        red = pixels.data[i];
+        green = pixels.data[i+1];
+        blue = pixels.data[i+2];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            pixels.data[i+3] = 0;
+        }
+    }
+    return pixels;
+}
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
